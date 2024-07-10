3fae31f31ee8d00643a88046d7ee24008632757a
Jihye Park <deviloper.park@gmail.com>
2024-07-10 11:54:53 +0900

    윤년 테스트 완료

--- HelloWorld/HelloWorldTests/LeapYearTests.swift
+++ HelloWorld/HelloWorldTests/LeapYearTests.swift
@@ -8,14 +8,24 @@
 import XCTest
 
 func isLeap(_ year: Int) -> Bool {
-    return false
+    guard year % 400 != 0 else { return true }
+    guard year % 100 != 0 else { return false }
+    return year % 4 == 0
 }
 
-final class LeapYearTests: XCTestCase {
+final class HelloWorldTests: XCTestCase {
     func testEvenlyDivisibleBy4IsLeap() {
+        // true인지 확인
         XCTAssertTrue(isLeap(2020))
     }
-    func testEvenlyDivisibleBy100IsNotLeap() {}
-    func testEvenlyDivisibleBy400isLeap() {}
-    func testNotEvenlyDivisibleBy4Or400IsNotLeap() {}
+    func testEvenlyDivisibleBy100IsNotLeap() {
+        // false인지 확인
+        XCTAssertFalse(isLeap(2100))
+    }
+    func testEvenlyDivisibleBy400isLeap() {
+        XCTAssertTrue(isLeap(2000))
+    }
+    func testNotEvenlyDivisibleBy4Or400IsNotLeap() {
+        XCTAssertFalse(isLeap(2021))
+    }
 }
