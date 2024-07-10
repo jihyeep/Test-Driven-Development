0b1edf8aa84ec01169a5579f0747bc93bf4dd897
Jihye Park <deviloper.park@gmail.com>
2024-07-10 14:09:05 +0900

    groupMenuByCategory 1차 테스트

--- Albertos/Albertos/MenuItem.swift
+++ Albertos/Albertos/MenuItem.swift
@@ -9,11 +9,16 @@ import Foundation
 
 struct MenuItem {
     let category: String
+    let name: String
 }
 
 func groupMenuByCategory(_ menu: [MenuItem]) -> [MenuSection] {
-    if menu.isEmpty {
-        return []
-    }
-    return [MenuSection()]
+    if menu.isEmpty { return [] }
+    return Dictionary(grouping: menu, by: { $0.category })
+        .map { key, value in
+            MenuSection(category: key, items: value)
+        }
+        .sorted {
+            $0.category > $1.category
+        } // 내림차순 정렬
 }
--- Albertos/AlbertosTests/MenuGroupingTests.swift
+++ Albertos/AlbertosTests/MenuGroupingTests.swift
@@ -19,11 +19,13 @@ final class MenuGroupingTests: XCTestCase {
     // 메뉴 아이템이 동일한 카테고리를 갖는 경우, 하나의 섹션으로 구분
     func testMenuWithOneCategoryReturnOneSection() {
         let menu = [
-            MenuItem(category: "pastas"),
-            MenuItem(category: "pastas")
+            MenuItem(category: "pastas", name: "a pasta"),
+            MenuItem(category: "drinks", name: "a drink"),
+            MenuItem(category: "pastas", name: "a pasta"),
+            MenuItem(category: "desserts", name: "a dessert")
         ]
         let sections = groupMenuByCategory(menu)
-        XCTAssertEqual(sections.count, 1) // Assert
+        XCTAssertEqual(sections.count, 3) // Assert
 
     }
     
