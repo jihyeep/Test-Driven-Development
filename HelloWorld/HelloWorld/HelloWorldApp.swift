b641390e1a726bd067bde4557532d1c6a3a3f01d
Jihye Park <deviloper.park@gmail.com>
2024-07-10 10:38:57 +0900

    XCUnitTest target 추가

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by 박지혜 on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
