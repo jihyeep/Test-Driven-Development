7f602d1e4be6a26b92d014526d62e673b28a0312
Jihye Park <deviloper.park@gmail.com>
2024-07-10 09:51:38 +0900

    Swift로 스크립트 파일 만들기

---
+++ hello_world.swift
@@ -0,0 +1,11 @@
+#!/user/bin/env xcrun swift
+
+func main() {
+    guard CommandLine.argc > 1 else {
+        print("Hello, world")
+        return
+    }
+    print("Hello, \(CommandLine.arguments[1])!")
+}
+
+main()
