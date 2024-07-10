d84c1535b1f2a7a0535f591ed57c2f777607c73a
Jihye Park <deviloper.park@gmail.com>
2024-07-10 13:13:37 +0900

    Test 함수 작성

---
+++ Albertos/AlbertosTests/MenuGroupingTests.swift
@@ -0,0 +1,27 @@
+//
+//  AlbertosTests.swift
+//  AlbertosTests
+//
+//  Created by 박지혜 on 7/10/24.
+//
+
+import XCTest
+@testable import Albertos
+
+final class MenuGroupingTests: XCTestCase {
+    
+    // 하나의 카테고리는 하나여야 함
+    func testMenuWithManyCategoriesReturnsOneSectionPerCategory() {
+        
+    }
+    
+    // 하나의 카테고리에 하나의 섹션
+    func testMenuWithOneCategoryReturnOneSection() {
+        
+    }
+    
+    // 비어 있는 섹션은 비어 있어야 함
+    func testEmptyMenuReturnsEmptySections() {
+        
+    }
+}
