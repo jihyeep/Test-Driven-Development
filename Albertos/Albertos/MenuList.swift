a1327a93e5b411ce4477aa09edab8d2f2df97f52
Jihye Park <deviloper.park@gmail.com>
2024-07-11 11:41:09 +0900

    MenuList.ViewModel 적용 및 MenuItem 의존 주입

--- Albertos/Albertos/MenuList.swift
+++ Albertos/Albertos/MenuList.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct MenuList: View {
-    let sections: [MenuSection]
+    let viewModel: ViewModel
     
     var body: some View {
         List {
-            ForEach(sections) { section in
+            ForEach(viewModel.sections) { section in
                 Section(header: Text(section.category)) {
                     ForEach(section.items) { item in
                         MenuRow(viewModel: .init(item: item))
@@ -24,5 +24,7 @@ struct MenuList: View {
 }
 
 #Preview {
-    MenuList(sections: groupMenuByCategory(menu))
+    NavigationStack {
+        MenuList(viewModel: .init(menu: menu, menuGrouping: groupMenuByCategory))
+    }
 }
